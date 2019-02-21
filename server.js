0a7d95b11b8c2c6935116ef353211f8ac3fab642
ehoversten <ehoversten@gmail.com>
2019-02-20 19:32:32 -0500

    finished backend, added Express Router and basic GET(all), POST(item), DELETE(item) routes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
+// api/items/*routes
+const items = require('./routes/api/items');
+
 const app = express();
 
 // BodyParser Middlewear
@@ -16,6 +19,10 @@ mongoose
     .then(() => console.log("MongoDB Connected ... "))
     .catch(err => console.log(err));
     
+
+// Use Routes
+app.use('/api/items', items);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
